5fb9af91da437883d147890b03fd940c2e718819
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-15 01:15:40 -0700

    Initial commit

---
+++ src/main/java/com/ninabornemann/spring_data_asterixapi/SpringDataAsterixApiApplication.java
@@ -0,0 +1,13 @@
+package com.ninabornemann.spring_data_asterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataAsterixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ninabornemann/spring_data_asterixapi/SpringDataAsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninabornemann.spring_data_asterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAsterixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
