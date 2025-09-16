99273b1f9eb9e20e855ba156eade5bc26e9c3211
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-16 03:33:50 -0700

    add strict tests and idService mock for addCharacter

--- src/test/java/com/ninabornemann/spring_data_asterixapi/service/AsterixServiceTest.java
+++ src/test/java/com/ninabornemann/spring_data_asterixapi/service/AsterixServiceTest.java
@@ -14,7 +14,7 @@ import static org.mockito.Mockito.*;
 class AsterixServiceTest {
 
     @Test
-    void getCharacters() {
+    void getCharacters_shouldReturnAll() {
         CharacterRepo mockRepo = mock(CharacterRepo.class);
         IdService mockIdService = mock(IdService.class);
         AsterixService service = new AsterixService(mockRepo, mockIdService);
@@ -27,39 +27,49 @@ class AsterixServiceTest {
 
         assertEquals(response, actual);
         verify(mockRepo).findAll();
+        verifyNoMoreInteractions(mockRepo);
     }
 
     @Test
-    void deleteCharacterById() {
+    void deleteCharacterById_shouldDelete() {
         CharacterRepo mockRepo = mock(CharacterRepo.class);
         IdService mockIdService = mock(IdService.class);
         AsterixService service = new AsterixService(mockRepo, mockIdService);
         Characters c1 = new Characters("001", "Pancake", 29, "Hacker");
         Characters c2 = new Characters("002", "Cupcake", 25, "Scientist");
         List<Characters> charactersList = List.of(c1, c2);
+
+        doNothing().when(mockRepo).deleteById(anyString());
         when(mockRepo.existsById("001")).thenReturn(false);
+
         boolean actual = service.deleteCharacterById("001");
         assertFalse(actual);
         verify(mockRepo).deleteById("001");
+        verify(mockRepo).existsById("001");
+
+        verifyNoMoreInteractions(mockRepo);
     }
 
     @Test
-    void getCharacterById() {
+    void getCharacterById_shouldGetOneCharacter() {
         CharacterRepo mockRepo = mock(CharacterRepo.class);
         IdService mockIdService = mock(IdService.class);
         AsterixService service = new AsterixService(mockRepo, mockIdService);
         Characters c1 = new Characters("001", "Pancake", 29, "Hacker");
         Characters c2 = new Characters("002", "Cupcake", 25, "Scientist");
         List<Characters> charactersList = List.of(c1, c2);
+
         Optional<Characters> response = Optional.of(c1);
         when(mockRepo.findById("001")).thenReturn(response);
+
         Characters actual = service.getCharacterById("001");
         assertEquals(c1, actual);
         verify(mockRepo).findById("001");
+        verifyNoMoreInteractions(mockRepo);
     }
 
     @Test
-    void updateCharacterById() {
+    void updateCharacterById_shouldUpdateFields() {
         CharacterRepo mockRepo = mock(CharacterRepo.class);
         IdService mockIdService = mock(IdService.class);
         AsterixService service = new AsterixService(mockRepo, mockIdService);
@@ -78,5 +88,24 @@ class AsterixServiceTest {
         assertEquals("001", updatedCharacter.getId());
         verify(mockRepo).findById("001");
         verify(mockRepo).save(existing);
+        verifyNoMoreInteractions(mockRepo);
+    }
+
+    @Test
+    void addCharacter_shouldAddNewCharacter_withUUID() {
+        CharacterRepo mockRepo = mock(CharacterRepo.class);
+        IdService mockIdService = mock(IdService.class);
+        AsterixService service = new AsterixService(mockRepo, mockIdService);
+        CharacterDto characterToAdd = new CharacterDto("Cupcake", 22, "Scientist");
+        Characters addedCharacter = new Characters("012345CC","Cupcake", 22, "Scientist");
+
+        when(mockIdService.randomId()).thenReturn("012345CC");
+        when(mockRepo.insert(addedCharacter)).thenAnswer(i -> i.getArgument(0));
+
+        assertEquals(addedCharacter, service.addCharacter(characterToAdd));
+        verify(mockRepo).insert(addedCharacter);
+        verify(mockIdService).randomId();
+        verifyNoMoreInteractions(mockRepo);
+        verifyNoMoreInteractions(mockIdService);
     }
 }
